6e7257e0507d112fd4d7466f6baf5b898c61ce90
ALipovik <lipovik@gmai.com>
2022-03-07 22:28:31 +0300

    All not problem

--- lib/main.dart
+++ lib/main.dart
@@ -13,15 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: '12345',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -31,15 +22,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -52,47 +34,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Align(
@@ -117,8 +70,9 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Center(
               child: ConstrainedBox(
-                constraints: BoxConstraints.tightFor(width: 200, height: 100),
-                child: Text('constrainedBox TEXT',
+                constraints:
+                    const BoxConstraints.tightFor(width: 200, height: 100),
+                child: const Text('constrainedBox TEXT',
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.center,
                     style: TextStyle(fontSize: 40)),
@@ -127,42 +81,42 @@ class _MyHomePageState extends State<MyHomePage> {
             Container(
               color: Colors.lightGreenAccent,
               alignment: Alignment.topRight,
-              padding: EdgeInsets.all(30),
-              margin: EdgeInsets.only(top: 40),
-              child: Text(
+              padding: const EdgeInsets.all(30),
+              margin: const EdgeInsets.only(top: 40),
+              child: const Text(
                 'I need Help Container',
                 textDirection: TextDirection.ltr,
               ),
             ),
             Container(
-                padding: EdgeInsets.all(50),
+                padding: const EdgeInsets.all(50),
                 color: Colors.red,
                 child: Column(
                   textDirection: TextDirection.ltr,
                   crossAxisAlignment: CrossAxisAlignment.start,
                 )),
             Container(
-                padding: EdgeInsets.all(40),
-                color: Color.fromARGB(200, 100, 0, 98),
+                padding: const EdgeInsets.all(40),
+                color: const Color.fromARGB(200, 100, 0, 98),
                 child: Column(
                   children: <Widget>[
-                    Text('Этот текст первый в тесте COLUMN,',
+                    const Text('Этот текст первый в тесте COLUMN,',
                         textDirection: TextDirection.ltr),
-                    Text('Этот текст второй в тексте.',
+                    const Text('Этот текст второй в тексте.',
                         textDirection: TextDirection.ltr),
-                    Text('была проблема с двумя виджетами,',
+                    const Text('была проблема с двумя виджетами,',
                         textDirection: TextDirection.ltr),
-                    Text('Она решилась благодаря больше меньше',
+                    const Text('Она решилась благодаря больше меньше',
                         textDirection: TextDirection.ltr),
                     Container(
-                        padding: EdgeInsets.all(30),
+                        padding: const EdgeInsets.all(30),
                         color: Colors.blue,
                         child: Row(
                           textDirection: TextDirection.ltr,
                           crossAxisAlignment: CrossAxisAlignment.center,
                           verticalDirection: VerticalDirection.down,
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
+                          children: const <Widget>[
                             Text('ROW TEST TEXT',
                                 textDirection: TextDirection.ltr),
                             Text('Test mini', textDirection: TextDirection.ltr),
