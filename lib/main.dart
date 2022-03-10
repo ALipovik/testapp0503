45961f5ebbddc420979446e1b2c68f4d3656b4cd
ALipovik <lipovik@gmai.com>
2022-03-10 10:40:57 +0300

    Testapp TEXT

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -41,100 +43,19 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Align(
-                alignment: Alignment.centerLeft,
-                child: Text('Alignment Text Test')),
-            const Center(
-              child: Text(
-                'Text Center Align',
-                textDirection: TextDirection.rtl,
-              ),
-            ),
-            const Padding(
-              padding:
-                  EdgeInsets.only(top: 40, bottom: 30, left: 20, right: 20),
-              child: Align(
-                alignment: Alignment.bottomLeft,
-                child: Text(
-                  'NEW TEXT PADDING',
-                  textDirection: TextDirection.ltr,
-                ),
-              ),
-            ),
-            Center(
-              child: ConstrainedBox(
-                constraints:
-                    const BoxConstraints.tightFor(width: 200, height: 100),
-                child: const Text('constrainedBox TEXT',
-                    textDirection: TextDirection.ltr,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 40)),
-              ),
-            ),
-            Container(
-              color: Colors.lightGreenAccent,
-              alignment: Alignment.topRight,
-              padding: const EdgeInsets.all(30),
-              margin: const EdgeInsets.only(top: 40),
-              child: const Text(
-                'I need Help Container',
-                textDirection: TextDirection.ltr,
-              ),
-            ),
-            Container(
-                padding: const EdgeInsets.all(50),
-                color: Colors.red,
-                child: Column(
-                  textDirection: TextDirection.ltr,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                )),
-            Container(
-                padding: const EdgeInsets.all(40),
-                color: const Color.fromARGB(200, 100, 0, 98),
-                child: Column(
-                  children: <Widget>[
-                    const Text('Этот текст первый в тесте COLUMN,',
-                        textDirection: TextDirection.ltr),
-                    const Text('Этот текст второй в тексте.',
-                        textDirection: TextDirection.ltr),
-                    const Text('была проблема с двумя виджетами,',
-                        textDirection: TextDirection.ltr),
-                    const Text('Она решилась благодаря больше меньше',
-                        textDirection: TextDirection.ltr),
-                    Container(
-                        padding: const EdgeInsets.all(30),
-                        color: Colors.blue,
-                        child: Row(
-                          textDirection: TextDirection.ltr,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          verticalDirection: VerticalDirection.down,
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: const <Widget>[
-                            Text('ROW TEST TEXT',
-                                textDirection: TextDirection.ltr),
-                            Text('Test mini', textDirection: TextDirection.ltr),
-                            Text('NEW TeXt', textDirection: TextDirection.ltr),
-                            Text('GoodLuck', textDirection: TextDirection.ltr),
-                          ],
-                        ))
-                  ],
-                ))
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Container(
+          padding: const EdgeInsets.only(top: 40, left: 20, right: 20),
+          color: Colors.green,
+          child: const Text('Helllo too Text new Test',
+              textDirection: TextDirection.ltr,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 30,
+                  backgroundColor: Colors.yellow)),
+        ));
   }
 }
